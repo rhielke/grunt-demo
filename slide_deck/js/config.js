814fdc5bf06bbc7349bbdbc7a3dc7113e70e9c1c
Scott Addie <tobias.addie@gmail.com>
2015-03-29 20:26:04 -0500

    Added slide deck

---
+++ slide_deck/js/config.js
@@ -0,0 +1,100 @@
+var presentationConfig = {
+	"dependencies": [
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		},
+		{
+			"enabled": false
+		}
+	]
+};
---
+++ slide_deck/js/files.js
@@ -0,0 +1,24 @@
+var presentationFiles = [
+	{
+		"_id": "55032fa6e279a5711300000f",
+		"updatedAt": "2015-03-30T00:51:00.841Z",
+		"url": "",
+		"content": "{\n  \"name\": \"GruntDemoApp\",\n  \"version\": \"1.0.0\",\n  \"description\": \"ASP.NET MVC sample app using Grunt\",\n  \"author\": \"Scott Addie <tobias.addie@gmail.com>\",\n  \"license\": \"MIT\",\n  \"devDependencies\": {\n    \"grunt\": \"^0.4.5\"\n  }\n}",
+		"mimeType": "text/plain",
+		"language": "application/json",
+		"folder": false,
+		"parent": null,
+		"name": "package.json"
+	},
+	{
+		"_id": "55033130f248f27d1300000b",
+		"updatedAt": "2015-03-30T00:51:00.841Z",
+		"url": "",
+		"content": "{\n  \"name\": \"GruntDemoApp\",\n  \"version\": \"1.0.0\",\n  \"description\": \"ASP.NET MVC sample app using Grunt\",\n  \"author\": \"Scott Addie <tobias.addie@gmail.com>\",\n  \"license\": \"MIT\",\n  \"devDependencies\": {\n    \"grunt\": \"^0.4.5\",\n    \"grunt-contrib-jshint\": \"^0.11.0\"\n  }\n}\n",
+		"mimeType": "text/plain",
+		"language": "application/json",
+		"folder": false,
+		"parent": null,
+		"name": "package_new.json"
+	}
+];
