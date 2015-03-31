abff3a7002d1fecc4f9f54b6a4619f791f399cbf
Scott Addie <tobias.addie@gmail.com>
2015-03-30 21:03:21 -0500

    Upgraded to latest version of MVC and adjusted minification/bundling code

--- GruntDemoApp/App_Start/BundleConfig.cs
+++ GruntDemoApp/App_Start/BundleConfig.cs
@@ -8,24 +8,22 @@ namespace GruntDemoApp
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/vendorBodyJs").Include(
+                        "~/Scripts-Build/jquery-{version}.min.js",
+                        "~/Scripts-Build/jquery.validate*",
+                        "~/Scripts-Build/bootstrap.min.js",
+                        "~/Scripts-Build/respond.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/vendorHeadJs").Include(
+                        "~/Scripts-Build/modernizr-*"));
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap/bootstrap.css",
-                      "~/Content/my/site.css"));
+                      "~/Content-Build/bootstrap/bootstrap.min.css",
+                      "~/Content-Build/my/site.min.css"));
         }
     }
 }
--- GruntDemoApp/Gruntfile.js
+++ GruntDemoApp/Gruntfile.js
@@ -1,4 +1,4 @@
-﻿/// <vs BeforeBuild='build' />
+/// <vs BeforeBuild='build' />
 /*global module*/
 module.exports = function (grunt) {
     "use strict";
